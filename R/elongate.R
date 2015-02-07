4bd7d5476fd283bff47b9bc4f4ff9b9f2eba3e75
Xavier Robin <xavalias-github@xavier.robin.name>
2015-02-07 14:54:10 +0100

    Initial commit after the meetup

---
+++ R/elongate.R
@@ -0,0 +1,27 @@
+#' @title Elongates a pseudo-wide column
+#' @description Takes a pseudo-wide column and tranform it into a long format
+#' @param id a column that will not be splitted
+#' @param wide the column to split
+#' @param pattern the character or regex used to split up the wide column (see \link{str_split})
+#' @examples 
+#' data(pseudo.wide)
+#' elongated.score <- elongate(pseudo.wide$Protein, pseudo.wide$Score)
+#' elongated.sequence <- elongate(pseudo.wide$Protein, pseudo.wide$Sequence)
+#' # The following works only if the Score and Sequence have the same pattern
+#' # (ie same number of splits per ID)
+#' elongated <- data.frame(Protein = elongated.score$id, 
+#'                         Score = elongated.score$wide,
+#'                         Sequence = elongated.sequence$wide)
+#' \dontshow{
+#' stopifnot(identical(dim(elongated.score), dim(elongated.sequence)))
+#' }
+#' @importFrom stringr str_split
+#' @export
+elongate <- function(id, wide, pattern = ";") {
+  splitted <- str_split(wide, pattern)
+  long <- data.frame(id = rep.int(id, sapply(splitted, length)), wide = unlist(splitted))
+  return(long)
+}
+
+
+
---
+++ R/pseudo.wide.R
@@ -0,0 +1,7 @@
+#' A dummy data set in pseudo-wide format
+#' @docType data
+#' @name pseudo.wide
+#' @description This data set list proteins with two pseudo-wide columns
+#' containing the identified sequence and associate score
+#' @format a \code{data.frame} with 3 columns.
+NULL
