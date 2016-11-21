ca304116457e7ecfea64b3aa95e37be65bb0e48b
Xavier Robin <xavalias-github@xavier.robin.name>
2016-11-21 10:47:30 +0100

    autoParApply

---
+++ R/AutoParApply.R
@@ -0,0 +1,46 @@
+#' *apply with auto-parallelization if available
+#' autoPar*apply functions guess the number of cores to use and
+#' run in parallel if possible.
+#' @rdname autoParApply
+#' @param X,FUN,... arguments to \code{\link{lapply}}
+#' @importFrom parallel parLapply
+#' @examples
+#' autoParLapply(1:10, function(x) x^2)
+#' @export
+autoParLapply <- function(X, FUN, ...) {
+	ncpus <- min(guessCores(), length(X))
+	if (ncpus > 1) {
+		if (!require(parallel)) {
+			stop("parallel package required with more than 1 cores")
+		}
+		# Do not start more than length(X) workers
+		cl <- makeCluster(ncpus)
+		ret <- parLapply(cl = cl, X, FUN, ...)
+		stopCluster(cl)
+		return(ret)
+	}
+	else {
+		return(lapply(X, FUN, ...))
+	}
+}
+
+#' @rdname autoParApply
+#' @examples
+#' autoParSapply(1:10, function(x) x^2)
+#' @export
+autoParSapply <- function(X, FUN, ...) {
+	ncpus <- min(guessCores(), length(X))
+	if (ncpus > 1) {
+		if (!require(parallel)) {
+			stop("parallel package required with more than 1 cores")
+		}
+		# Do not start more than length(X) workers
+		cl <- makeCluster(ncpus)
+		ret <- parSapply(cl = cl, X, FUN, ...)
+		stopCluster(cl)
+		return(ret)
+	}
+	else {
+		return(sapply(X, FUN, ...))
+	}
+}
