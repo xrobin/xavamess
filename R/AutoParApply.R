268c035892c646a238202ec302366f2af880d39a
Xavier Robin <xavalias-github@xavier.robin.name>
2016-11-22 13:45:47 +0100

    Use the same code for both sapply and lapply

--- R/AutoParApply.R
+++ R/AutoParApply.R
@@ -8,6 +8,25 @@
 #' autoParLapply(1:10, function(x) x^2)
 #' @export
 autoParLapply <- function(X, FUN, ...) {
+	return(autoParXapply(list(lapply, parLapply), X=X, FUN=FUN, ...))
+}
+
+
+#' @rdname autoParApply
+#' @examples
+#' autoParSapply(1:10, function(x) x^2)
+#' @export
+autoParSapply <- function(X, FUN, ...) {
+	return(autoParXapply(list(sapply, parSapply), X=X, FUN=FUN, ...))
+}
+
+
+# This is the generic function
+# It takes an extra argument .XFUN which is a list
+# containing the two functions to use
+# eg autoParXapply(list(sapply, parSapply), ...) for sapply
+# ... contains MARGIN and FUN
+autoParXapply <- function(.XFUN, X, ...) {
 	# Do not start more than length(X) workers
 	ncpus <- min(guessCores(), length(X))
 	if (ncpus > 1) {
@@ -17,32 +36,11 @@ autoParLapply <- function(X, FUN, ...) {
 		cl <- makeCluster(ncpus)
 		# Export all .GlobalEnv
 		clusterExport(cl, ls(envir = .GlobalEnv), envir = .GlobalEnv)
-		ret <- parLapply(cl = cl, X, FUN, ...)
+		ret <- .XFUN[[2]](cl = cl, X, ...)
 		stopCluster(cl)
 		return(ret)
 	}
 	else {
-		return(lapply(X, FUN, ...))
-	}
-}
-
-#' @rdname autoParApply
-#' @examples
-#' autoParSapply(1:10, function(x) x^2)
-#' @export
-autoParSapply <- function(X, FUN, ...) {
-	ncpus <- min(guessCores(), length(X))
-	if (ncpus > 1) {
-		if (!require(parallel)) {
-			stop("parallel package required with more than 1 cores")
-		}
-		# Do not start more than length(X) workers
-		cl <- makeCluster(ncpus)
-		ret <- parSapply(cl = cl, X, FUN, ...)
-		stopCluster(cl)
-		return(ret)
-	}
-	else {
-		return(sapply(X, FUN, ...))
+		return(.XFUN[[1]](X, FUN, ...))
 	}
 }
