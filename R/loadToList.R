bdf18132027308e1c20b80a0ce641dee9930a913
Xavier Robin <xavalias-github@xavier.robin.name>
2016-11-23 14:19:47 +0100

    Add loadToEnv function

--- R/loadToList.R
+++ R/loadToList.R
@@ -1,6 +1,9 @@
-#' Load an R dataset written with the \code{\link{save}} function into a \code{\link{list}}
+#' Load to a list of environment
+#' Loads an R dataset written with the \code{\link{save}} function into a \code{\link{list}} or \code{\link{environment}}
+#' @rdname loadTo
+#' @name loadTo
 #' @param file the \code{\link{file}} argument for \code{\link{load}}, see there for more details
-#' @return a list with the named contents of \code{\link{file}}
+#' @return a list or environment with the named contents of \code{\link{file}}
 #' @examples
 #' fname <- tempfile()
 #' a <- "b"
@@ -18,4 +21,22 @@ loadToList <- function(file) {
 		ret[[object.name]] <- get(object.name)
 	}
 	return(ret)
+}
+
+#' @rdname loadTo
+#' @examples
+#' e <- loadToEnv(fname)
+#' \dontshow{
+#' testEnv <- new.env()
+#' assign("a", "b", envir = testEnv)
+#' assign("b", 5, envir = testEnv)
+#' # Can't use identical on environment
+#' stopifnot(isTRUE(all.equal(e, testEnv)))
+#' }
+#' file.remove(fname)
+#' @export
+loadToEnv <- function(file) {
+	env <- new.env(parent = .GlobalEnv)
+	loaded.object.names <- load(file, envir = env)
+	return(env)
 }
