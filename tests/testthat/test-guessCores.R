f196490df002e7834f74486a022876fcdc683a38
Xavier Robin <xavalias-github@xavier.robin.name>
2017-02-05 12:40:50 +0100

    Better test

--- tests/testthat/test-guessCores.R
+++ tests/testthat/test-guessCores.R
@@ -7,7 +7,7 @@ expect_true(is.integer(ncores), label = "ncores is an integer")
 expect_gt(ncores, 0, label = "ncores is positive")
 
 context("guessCores output is sensible")
-expect_output(guessCores(verbose = FALSE), NA, label = "no output with verbose=FALSE")
+expect_silent(guessCores(verbose = FALSE))
 expect_output(guessCores(verbose = TRUE), sprintf(" %d ", ncores), label = "output contains the number of cores with verbose=TRUE")
 
 context("guessCores's 'max' argument works")
