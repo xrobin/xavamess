64d84a3e6c9ba99df1c34b8778e5e11495c40f67
Xavier Robin <xavalias-github@xavier.robin.name>
2017-03-04 16:27:46 +0100

    Cleanup

--- tests/testthat/test-mktemp.R
+++ tests/testthat/test-mktemp.R
@@ -1,23 +1,27 @@
-library(xavamess)
+context("mktemp")
 
-context("mktemp can create a basic temp file")
-tmpfile <- mktemp()
-expect_true(file.exists(tmpfile))
-file.remove(tmpfile)
-expect_false(file.exists(tmpfile))
-
-context("mktemp rejects illegal templates")
-illegal.templates <- c(NA, "", "a", "aa", "aaa", "aa-XXX", "aa-XXX.", "aa-XXX-")
-expect_error(mktemp(illegal.templates))
-lapply(illegal.templates, function(i) {
-	expect_error(mktemp(i))
-})
-
-context("mktemp works with legal templates")
-legal.templates <- c("XXX", "a.XXX")
-lapply(legal.templates, function(i) {
-	tmpfile <- mktemp(i)
+test_that("mktemp can create a basic temp file", {
+	tmpfile <- mktemp()
 	expect_true(file.exists(tmpfile))
 	file.remove(tmpfile)
 	expect_false(file.exists(tmpfile))
+})
+
+
+test_that("mktemp rejects illegal templates", {
+	illegal.templates <- c(NA, "", "a", "aa", "aaa", "aa-XXX", "aa-XXX.", "aa-XXX-")
+	expect_error(mktemp(illegal.templates))
+	lapply(illegal.templates, function(i) {
+		expect_error(mktemp(i))
+	})
+})
+
+test_that("mktemp works with legal templates", {
+	legal.templates <- c("XXX", "a.XXX")
+	lapply(legal.templates, function(i) {
+		tmpfile <- mktemp(i)
+		expect_true(file.exists(tmpfile))
+		file.remove(tmpfile)
+		expect_false(file.exists(tmpfile))
+	})
 })
