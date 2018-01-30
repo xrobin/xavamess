905f0894cd724bb44233370ddea330ff058ff276
Xavier Robin <xavalias-github@xavier.robin.name>
2018-01-30 15:35:02 +0100

    Add getMart function to simplify the use of biomaRt with Ensembl

---
+++ R/getMart.R
@@ -0,0 +1,41 @@
+#' Get the Mart corresponding the Ensembl version and species
+#' @param version the ensembl version (number) or \dQuote{current}
+#' @param species the species, currently either \dQuote{human} or \dQuote{mouse} in English, or the species part of the dataset name (without the \dQuote{_gene_ensembl} part, as listed by \code{\link[biomaRt]{listDatasets}}, for instance \dQuote{rnorvegicus} or \dQuote{scerevisiae}).
+#' @examples
+#' library(biomaRt)
+#' ensembl.proteins <- c("ENSP00000361930", "ENSP00000309503")
+#' mart <- getMart(88, "human")
+#' getBM(attributes = c("ensembl_peptide_id", "uniprotswissprot", "refseq_peptide", "hgnc_symbol"), filters = "ensembl_peptide_id", values = ensembl.proteins, mart = mart)
+#' 
+#' # Mouse example
+#' ensembl.proteins <- c("ENSMUSP00000004055", "ENSMUSP00000002708")
+#' mart <- getMart(88, "mmusculus")
+#' getBM(attributes = c("ensembl_peptide_id", "uniprotswissprot", "refseq_peptide", "mgi_symbol"), filters = "ensembl_peptide_id", values = ensembl.proteins, mart = mart)
+#' @export
+getMart <- function(version, species = "human") {
+  if (! requireNamespace("biomaRt", quietly = TRUE)) {
+    stop("Please install biomaRt from Bioconductor. See the the installation instructions: <https://doi.org/doi:10.18129/B9.bioc.biomaRt>")
+  }
+    
+  biomart <- "ENSEMBL_MART_ENSEMBL"
+
+  if (species == "human") {
+    dataset <- "hsapiens_gene_ensembl"
+  }
+  else if (species == "mouse") {
+    dataset <- "mmusculus_gene_ensembl"
+  }
+  else {
+    dataset <- paste0(species, "_gene_ensembl")
+  }
+  
+  mart <- biomaRt::useEnsembl("ensembl", dataset = dataset, version = version)
+  # Check we're using the right version
+  mart.list <- biomaRt::listMarts(mart)
+  if (mart.list$version[mart.list$biomart == biomart] != paste0("Ensembl Genes ", version)) {
+    stop("Got the wrong Ensembl version")
+  }
+  
+  return(mart)
+  
+}
---
+++ tests/testthat/test-getmart.R
@@ -0,0 +1,41 @@
+library(xavamess)
+
+context("getMart works")
+
+test_that("getMart works on human inputs", {
+	skip_if_not_installed("biomaRt")
+
+	library(biomaRt)
+	ensembl.proteins <- c("ENSP00000361930", "ENSP00000309503")
+	mart <- getMart(88, "human")
+	res <- getBM(attributes = c("ensembl_peptide_id", "uniprotswissprot", "refseq_peptide", "hgnc_symbol"), filters = "ensembl_peptide_id", values = ensembl.proteins, mart = mart)
+
+
+	order.input <- order(ensembl.proteins)
+	order.output <- order(res$ensembl_peptide_id)
+	expect_equal(ensembl.proteins[order.input], res$ensembl_peptide_id[order.output])
+	expect_equal(res$uniprotswissprot[order.output], c("P63104", "P31946"))
+	expect_equal(res$refseq_peptide[order.output], c("NP_003397", "NP_003395"))
+	expect_equal(res$hgnc_symbol[order.output], c("YWHAZ", "YWHAB"))
+})
+
+test_that("getMart works on mouse inputs", {
+	skip_if_not_installed("biomaRt")
+
+	library(biomaRt)
+	ensembl.proteins <- c("ENSMUSP00000004055", "ENSMUSP00000002708")
+	mart <- getMart(88, "mmusculus")
+	res <- getBM(attributes = c("ensembl_peptide_id", "uniprotswissprot", "refseq_peptide", "mgi_symbol"), filters = "ensembl_peptide_id", values = ensembl.proteins, mart = mart)
+
+	order.input <- order(ensembl.proteins)
+	order.output <- order(res$ensembl_peptide_id)
+	expect_equal(ensembl.proteins[order.input], res$ensembl_peptide_id[order.output])
+	expect_equal(res$uniprotswissprot[order.output], c("Q62226", "Q8BMD2"))
+	expect_equal(res$refseq_peptide[order.output], c("NP_033196", "NP_080219"))
+	expect_equal(res$mgi_symbol[order.output], c("Shh", "Dzip1"))
+})
+
+test_that("getMart crashes on in invalid species input", {
+	skip_if_not_installed("biomaRt")
+	expect_error(getMart(88, "nospecies"))
+})
